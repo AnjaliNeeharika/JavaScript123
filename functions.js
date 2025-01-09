27e1b907f9a778437a2d7a8dbdbf927d61d1ed40
Anjali Neeharika <tanjalineeharika@gmail.com>
2025-01-09 11:50:16 +0530

    add files

--- functions.js
+++ functions.js
@@ -14,10 +14,66 @@
 // add1(5,6)
 // add1(10,30)
 
-function Addtocart(){
-    alert("product will added to cart successfully");
-    document.body.style.background="red";
+// function Addtocart(){
+//     alert("product will added to cart successfully");
+//     document.body.style.background="red";
+// }
+
+//function with out parameters
+
+// function demo(){
+//     console.log("without parameters");
+// }
+// demo();
+
+//function with parameters
+
+function userdetails(name, age,city){
+    console.log(name);
+    console.log(age);
+    console.log(city);
+
 }
+userdetails("yeswanth",20,"hyderabad");
+
+//anonymus function
+
+// function(){
+//     console.log("anonymus function");
+// }()
+
+//function expresion
+let x=function(){
+  console.log("function expresion is executing");
+};
+x();
+
+
+//IIFE function
+(function(){
+    console.log("IIFE");
+  })();
+
+
+//arrow function
+// function demo(){
+//     console.log("hello");
+// }
+// demo();
+
+let n=_=>console.log("arrow function");
+n();
+let c=a=>console.log(a);
+c(5);
+let v=(a,b)=>console.log(a+b);
+v(5,6);
+
+let d=_=>console.log("hi");
+console.log("hello");
+console.log("bye");
+d();
+
+
 
 
 
